167988a127d4aa2ca60c583cc34e3bdc7a092c6f
mikerw <mikerw@live.unc.edu>
2016-07-11 13:32:05 -0400

    made new files for assignment 3

--- mikerw-convert-docs.sh
+++ mikerw-convert-docs.sh
@@ -1,13 +1,14 @@
-
+#!/bin/bash
 
 # Declare input argument as a variable
-read inputVar
+INPUTVAR=$1
 # Convert markdown to HTML
-
+pandoc -o $INPUTVAR.html $INPUTVAR.md
 # Convert markdown to DOCX
-
+pandoc -o $INPUTVAR.docx $INPUTVAR.md
 # Convert markdown to ODT
-
+pandoc -o $INPUTVAR.odt $INPUTVAR.md
 # Convert markdown to PDF
-
+pandoc -o $INPUTVAR.pdf $INPUTVAR.md
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted $INPUTVAR to HTML, DOCX, ODT, PDF"
